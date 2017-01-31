9b3fa8e9fd36d3cf3052c1fc571eba15c5947e0b
Megan Talbot <meganftalbot@gmail.com>
2017-01-31 08:53:39 -0700

    More diary tests, not finding correct css selector

---
+++ test/diary.js
@@ -0,0 +1,43 @@
+var assert    = require('chai').assert;
+var webdriver = require('selenium-webdriver');
+var test      = require('selenium-webdriver/testing');
+
+test.describe('testing diary', function() {
+  var driver;
+  this.timeout(10000);
+
+  test.beforeEach(function() {
+    driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .build();
+  })
+
+  test.afterEach(function() {
+    driver.quit();
+  })
+
+  test.xit('adds a selected food to table', function(){
+
+    driver.get('http://localhost:8080/foods.html');
+    // var foodsArray = JSON.stringify([{name: 'swimming', calories: '500'}]);
+    // driver.executeScript("window.localStorage.setItem('foods-calories', '" + foodsArray + "');");
+
+    var foodName = driver.findElement({id: 'food-name'});
+    var foodCalories = driver.findElement({id: 'food-calories'});
+    var submitButton = driver.findElement({id: 'create-food'});
+
+    foodName.sendKeys('pizza');
+    foodCalories.sendKeys('500');
+    submitButton.click();
+
+    driver.get('http://localhost:8080/diary.html');
+
+    var foodsCheckBox = driver.findElement({css: '#foods #table-body tbody tr td:nth-of-type(1)'}).click();
+    var addToLunchButton = driver.findElement({css: '#lunch-button'})
+
+    addToLunchButton.click();
+    driver.findElement({css: '#lunch tr:nth-child(1) td:nth-child(1)'}).getText().then(function(textValue){
+      assert.equal(textValue, 'pizza')
+    });
+  });
+})
