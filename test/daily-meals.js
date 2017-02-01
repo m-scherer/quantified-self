14e54b1d43aedda10f793d42bbb22fd399eba85e
Michael Scherer <michael.c.scherer@gmail.com>
2017-01-31 18:08:18 -0700

    Add get all meals test

--- test/daily-meals.js
+++ test/daily-meals.js
@@ -5,7 +5,10 @@ var dailyMeals  = require('../lib/daily-meals');
 
 describe('DailyMeals testing', function() {
   it('can get all meals', function() {
-    mealsJSON = JSON.stringify([{date:01/30/2017,data:{breakfast:[{name:Apples,calories:40}],lunch:[{name:Peaches,calories:20}],dinner:[{name:Pasta,calories:400}],snack:[{name:Broccoli,calories:15}],exercise:[{name:Jumping,calories:500}]}}])
-    debugger;
+    data = [ {date: '01/30/2017', data: {breakfast: [], lunch: [], dinner: [], snack: [], exercise: []}} ]
+    mealsJSON = JSON.stringify(data);
+    meals = JSON.parse(mealsJSON);
+    assert.isArray(meals)
+     assert.deepEqual(meals, data)
   });
 });
